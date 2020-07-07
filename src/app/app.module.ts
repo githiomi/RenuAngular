b6e892587e8d460d64609d184a5385f34c5d5a6f
githiomi <danielgithiomi@gmail.com>
2020-07-07 23:52:33 +0300

    first master commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrialComponent } from './components/trial/trial.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrialComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
